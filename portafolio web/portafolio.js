d0e65e420b4790443141728b87556c5277922bc8
Santos0214 <96590965+Santos0214@users.noreply.github.com>
2022-07-04 01:46:45 +0200

    Add files via upload

---
+++ portafolio web/portafolio.js
@@ -0,0 +1,110 @@
+const menu = document.querySelector(".menu");
+const openMenuBtn = document.querySelector(".open-menu");
+const closeMenuBtn = document.querySelector(".close-menu");
+
+function toggleMenu() {
+  menu.classList.toggle("menu_opened");
+}
+
+openMenuBtn.addEventListener("click", toggleMenu);
+closeMenuBtn.addEventListener("click", toggleMenu);
+const menuLinks = document.querySelectorAll('.menu a[href^="#"]')
+
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach(entry => {
+       const id = entry.target.getAttribute("id");
+        const menuLink = document.querySelector(`.menu a[href="#${id}"]`);
+        
+        if(entry.isIntersecting){
+            document.querySelector(".menu a.selected").classList.remove("selected");
+            menuLink.classList.add("selected");
+           }
+    })
+    
+},{rooMargin:"-30% 0px -70% 0px"});
+
+menuLinks.forEach(menuLink => {
+    menuLink.addEventListener("click", function(){
+        menu.classList.remove("menu_opened");
+    })
+    
+    const hash = menuLink.getAttribute("href");
+    const targe = document.querySelector(hash);
+    
+    if(targe){
+        observer.observe(targe);
+    }
+    
+})
+
+
+function getData(){
+  
+  var nombre = document.getElementById('nombre').value;
+  var email = document.getElementById('email').value;
+  var mensaje = document.getElementById('mensaje').value;
+  if(nombre=='' ||  email=="" || mensaje==''){
+    alert('Lo campo estan vacio ');
+  }else if(validarEmail(email)){
+  
+  window.location.replace('enviar.html');   
+  
+  document.write('<!DOCTYPE html>');
+  document.write('<html lang="en">');
+  document.write('<head>');
+  document.write('<meta charset="UTF-8">');
+  document.write('<meta http-equiv="X-UA-Compatible" content="IE=edge">');
+  document.write('<meta name="viewport" content="width=device-width, initial-scale=1.0">');
+  document.write('<link rel="stylesheet" href="css/foto.css">');
+  document.write('<title>Gracias</title>');
+  document.write('</head>');
+  document.write('<body>');
+  document.write('<div class="contenedor">');
+  document.write('<div class="conImg">');
+  document.write('<img src="./img/WhatsApp Image 2022-06-30 at 4.45.17 AM.jpeg" alt="">');
+  document.write('</div>');
+  document.write('<p class="frase4">Gracias por inscribirse, le<br/>saluda Cristhian Santos Nazareno</p>');
+  document.write('<p class="frase1"> '+nombre+' revise su correo '+email+' para confirmar su registro </p>');
+  document.write('<p class="frase2">gracias por su comentario  '+mensaje+'</p>');
+  document.write('<div class="btn">');
+  document.write('<a href="contacto.html">Aceptar </a>');
+  document.write('</div>');
+  document.write('</div>');
+  document.write('</body>');
+  document.write('</html>');
+  
+}else{
+  window.location.replace('menu.html');  
+  alert("El email NO es correcto");
+}
+}
+
+
+function soloLetras(e){
+  key = e.keyCode || e.which;
+
+tecla = String.fromCharCode(key).toLowerCase();
+
+
+letras = " áéíóúabcdefghijklmnñopqrstuvwxyz";
+especiales = "8-37-39-46";
+
+tecla_especial = false
+ 
+  for(var i in especiales){
+      if(key == especiales[i]){
+           tecla_especial = true;
+           break;
+      }
+  }
+
+  if(letras.indexOf(tecla)==-1 && !tecla_especial){
+       return false;
+  }
+}
+
+function validarEmail(email) 
+{
+    var regex = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    return regex.test(email) ? true : false;
+}
